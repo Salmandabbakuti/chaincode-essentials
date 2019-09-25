142e511cf3846e79c28c56bc909226c7eebd5bc3
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-25 08:20:20 +0530

    Adding CLI container on start up

--- basic-network/start.sh
+++ basic-network/start.sh
@@ -25,3 +25,7 @@ sleep ${FABRIC_START_TIMEOUT}
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 # Join peer0.org1.example.com to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
+
+#Creating CLI Container
+sudo docker-compose -f ./docker-compose.yml up -d cli
+
